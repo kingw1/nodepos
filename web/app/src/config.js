4a376e2fa5b2ef5910df826b65804fa7f75c3b8f
King <wiwichai@gmail.com>
2024-04-27 14:02:37 +0700

    fix bug token issue

--- web/app/src/config.js
+++ web/app/src/config.js
@@ -1,6 +1,13 @@
 const config = {
   api_path: "http://localhost:3000",
-  token_name: "POS_TOKEN",
+  token_name: "NODEPOS_TOKEN",
+  headers: () => {
+    return {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("NODEPOS_TOKEN"),
+      },
+    };
+  },
 };
 
 export default config;
